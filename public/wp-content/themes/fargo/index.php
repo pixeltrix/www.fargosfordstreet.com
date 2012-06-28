5bb008414e403c3585dfdffc60e6ded004452621
Andrew White <andyw@pixeltrix.co.uk>
2012-06-28 06:34:28 +0100

    Add initial placeholder theme

---
+++ public/wp-content/themes/fargo/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+<meta name="viewport" content="width=device-width" />
+<title><?php bloginfo('name'); ?></title>
+<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+<?php wp_head(); ?>
+</head>
+
+<body>
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ public/wp-content/themes/fargo/placeholder.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+<meta name="viewport" content="width=device-width" />
+<title><?php bloginfo('name'); ?></title>
+<script type="text/javascript" src="http://use.typekit.com/jgp5dwx.js"></script>
+<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+<link rel="stylesheet" type="text/css" media="all" href="<?php echo get_template_directory_uri(); ?>/placeholder.css" />
+</head>
+
+<body>
+<div id="wrapper">
+<div id="header" class="container">
+<h1 id="site-title"><a href="<?php echo home_url('/'); ?>" title="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
+</div>
+<div id="content" class="container">
+<div class="columns">
+<div class="column span_12">
+<p class="intro">Far Gosford Street is one of Coventry&rsquo;s most historic surviving streets and is undergoing a huge regeneration with many listed buildings being brought back to former glories. Medieval buildings now stand proudly alongside 18th century top shops, and offer people the chance to shop in style. But it&rsquo;s much more than just the buildings, as good as they are! Always a hot bed of industry and creativity, Far Gosford Street is now home to many independent, unique businesses that are turning it into Coventry&rsquo;s Creative Quarter. And there&rsquo;s more to come&hellip;</p>
+</div>
+<div class="column span_6">
+<h2>Fargo Village</h2>
+<p>The &lsquo;jewel in the crown&rsquo; for independent shopping in Coventry! Affordable rents in Fargo Village will enable many artistic, creative and alternative businesses to set up and thrive. With artist&rsquo;s studios, design and make units for crafts, alongside a wide variety of alternative retail, Fargo Village will provide a home for these businesses that tend not to fare so well in more traditional high streets or shopping centres.</p>
+<p>And with cafes, a bar and a venue alongside events, festivals and entertainment every weekend, it&rsquo;s not all about spending money but having fun whilst checking out the best that Coventry has to offer. Fargo Village will be the destination for independent shopping for Coventry and the Midlands. Shopping as it should be, a fun experience &ndash; not a drag!</p>
+<h2>Hotel</h2>
+<p>With all the increased activity and nightlife that will envelop Far Gosford Street, it will become a destination not just for the day but, with everything else Coventry has to offer, for a weekend or more! To enable people to spend more time  in this unique street a brand new independent &lsquo;boutique style&rsquo; hotel is planned, situated close to the University. In keeping with the character of Far Gosford Street this will be visually stunning and provide a fantastic gateway into the Street.</p>
+<h2>Business</h2>
+<p>At a glance you may think  Far Gosford Street is pretty much shopping and leisure, however, dig beneath the surface and you will find many creative businesses hidden away, busy in the fields of IT, design and media. More are looking to set up in the area, adding to the creativity of Far Gosford Street, and so more office space is planned, within Fargo Village and also along Sky Blue Way.</p>
+<p>As well as providing dedicated space for creative enterprises the Sky Blue Way offices will also provide a new more attractive frontage to the road as it approaches the City Centre.</p>
+</div>
+<div class="column span_6">
+<h2>City/Student Living</h2>
+<p>With the University literally at the end of the Street, a great deal of student accommodation can be found in and around Far Gosford Street. This makes for a busy, vibrant street with many people already living on the street and using the businesses here. The start of the next University year will see more  accommodation above some of the recently refurbished shop units bringing more people into Far Gosford Street, and this will be increased further when a new housing project, Kilby Mews, is completed adding around thirty 3 & 4 bedroom homes behind the Hand and Heart.</p>
+<h2>Retail</h2>
+<p>Since medieval times Far Gosford Street has been a local shopping centre with independently run shops and businesses, and nothing really has changed! It is already home to many unique, small, specialist shops that offer personal service and goods that can&rsquo;t always be found on the high street. With the regeneration that has taken place and new retail units being built, a broader range of  retailers are already looking to set up in the street.</p>
+<p>And when  Fargo Village is up and running the number of shops and variety and choice of goods will increase considerably, bringing in shoppers from all over Coventry and beyond.</p>
+<h2>Nightlife</h2>
+<p>Already home to a variety of restaurants with food from around the world, as well as a range of takeaways, Far Gosford Street is certainly a destination for eating out, and more specialist eateries are planned to complement the several pubs and nightclub.</p>
+<p>Live music is a must and Far Gosford Street has one of Coventry&rsquo;s best loved pub venues, with bands from Coventry and beyond playing every weekend. With a new 700 person venue planned as part of Fargo Village there will also be bigger names in music, as well as theatre and comedy, coming to Coventry.</p>
+</div>
+</div>
+</div>
+<div id="footer">
+<div class="container">
+<div class="columns">
+<div class="column span_3">
+<p class="address">121-124 Far Gosford Street<br>Coventry CV1 5EA</p>
+<dl class="contact">
+<dt>Email:</dt>
+<dd><a href="mailto:fargo@coventry.gov.uk">fargo@coventry.gov.uk</a></dd>
+<dt>Telephone:</dt>
+<dd>024 7683 2643</dd>
+</dl>
+</div>
+<div class="column span_9">
+<ul class="logos">
+<li class="ccc"><a href="http://www.coventry.gov.uk" target="_blank" title="Coventry City Council">Coventry City Council</a></li>
+<li class="cdp"><a href="http://www.complexdevelopmentprojects.co.uk" target="_blank" title="Complex Development Projects">Complex Development Projects</a></li>
+<li class="erdf"><a href="http://www.communities.gov.uk/regeneration/regenerationfunding/europeanregionaldevelopment/" target="_blank" title="European Regional Development Fund">European Regional Development Fund</a></li>
+<li class="hlf"><a href="http://www.hlf.org.uk" target="_blank" title="Heritage Lottery Fund">Heritage Lottery Fund</a></li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+</div>
+</body>
+</html>
