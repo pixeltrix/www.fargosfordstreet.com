776a5e1a07f21655b9e7f197d4393518117725f9
Andrew White <andyw@pixeltrix.co.uk>
2012-10-03 03:15:50 +0100

    Utilise header and footer from placeholder

---
+++ public/wp-content/themes/fargo/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo('charset'); ?>" />
+<meta name="viewport" content="width=device-width" />
+<title><?php wp_title(''); ?></title>
+<script type="text/javascript" src="http://use.typekit.com/jgp5dwx.js"></script>
+<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo('stylesheet_url'); ?>" />
+<?php wp_head(); ?>
+</head>
+<body>
+<div id="wrapper">
+<div id="header" class="container">
+<h1 id="site-title"><a href="<?php echo home_url('/'); ?>" title="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
+</div>
+<div id="content" class="container">
--- public/wp-content/themes/fargo/index.php
+++ public/wp-content/themes/fargo/index.php
@@ -1,14 +1,2 @@
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-<head>
-<meta charset="<?php bloginfo( 'charset' ); ?>" />
-<meta name="viewport" content="width=device-width" />
-<title><?php bloginfo('name'); ?></title>
-<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
-<?php wp_head(); ?>
-</head>
-
-<body>
-<?php wp_footer(); ?>
-</body>
-</html>
+<?php get_header(); ?>
+<?php get_footer(); ?>
