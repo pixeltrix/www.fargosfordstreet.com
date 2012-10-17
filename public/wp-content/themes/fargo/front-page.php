8cc29457625176fee07e850c8119d25746cc03fd
Andrew White <andyw@pixeltrix.co.uk>
2012-10-17 12:30:34 +0100

    Add home page gallery and banner

--- public/wp-content/themes/fargo/front-page.php
+++ public/wp-content/themes/fargo/front-page.php
@@ -1,2 +1,11 @@
+<?php wp_enqueue_script('jquery'); ?>
+<?php wp_enqueue_script('jquery-tools'); ?>
 <?php get_header(); ?>
+<?php fargo_gallery(); ?>
+<div id="banner">
+<h1 class="site-title"><?php bloginfo( 'name' ); ?></h1>
+<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+</div>
+<div class="columns">
+</div>
 <?php get_footer(); ?>
--- public/wp-content/themes/fargo/functions.php
+++ public/wp-content/themes/fargo/functions.php
@@ -4,6 +4,11 @@
     register_nav_menus(array(
       'main' => 'Main Menu'
     ));
+
+    wp_register_script('jquery-tools', get_bloginfo('template_directory') . '/jquery.tools.js', array('jquery'), '1.2.7');
+
+    add_theme_support('post-thumbnails');
+    add_image_size('gallery', 900, 326, true);
   }
 
   add_action('after_setup_theme', 'fargo_theme_setup');
@@ -30,6 +35,46 @@
     if ($children) {
       echo "<ul class=\"page-menu\">\n{$children}\n</p>\n";
     }
+  }
+
+  function fargo_gallery() {
+    $post_id = get_the_ID();
+
+    $attachments = get_children(array(
+      'post_parent' => $post_id,
+      'post_status' => 'inherit',
+      'post_type' => 'attachment',
+      'post_mime_type' => 'image',
+      'order' => 'ASC', 'orderby' => 'menu_order ID'
+    ));
+
+    if (empty($attachments)) { return ''; }
+
+    $output = "<div id=\"gallery\">\n";
+    $images = "<div class=\"images\">\n";
+    $tabs = "<div class=\"tabs\">\n";
+
+    foreach ($attachments as $id => $attachment) {
+      $imgtag = wp_get_attachment_image($id, 'gallery');
+      $images .= "<div class=\"image\">{$imgtag}</div>\n";
+      $tabs .= "<a href=\"#\"></a>\n";
+    }
+
+    $images .= "</div>\n";
+    $tabs .= "</div>\n";
+
+    $interval = 4000;
+
+    $output .= $images;
+    $output .= $tabs;
+    $output .= "<script type=\"text/javascript\">\n";
+    $output .= "jQuery(function($) {\n";
+    $output .= "  $('#gallery .tabs').tabs('.images > div', { effect: 'fade', fadeOutSpeed: 'slow', rotate: true }).slideshow({ autoplay: true, interval: {$interval}, clickable: false });\n";
+    $output .= "});\n";
+    $output .= "</script>\n";
+    $output .= "</div>\n";
+
+    echo $output;
   }
 
 ?>
