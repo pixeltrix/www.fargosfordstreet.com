6e3f3ff30b7d7d1c905588495ef040fb91b96759
Andrew White <andyw@pixeltrix.co.uk>
2013-01-07 13:53:51 +0000

    Add front page content

--- public/wp-content/themes/fargo/front-page.php
+++ public/wp-content/themes/fargo/front-page.php
@@ -7,5 +7,10 @@
 <h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 </div>
 <div class="columns">
+<?php while (have_posts()): the_post(); ?>
+<div class="column span_12">
+<?php the_content(); ?>
+</div>
+<?php endwhile; ?>
 </div>
 <?php get_footer(); ?>
