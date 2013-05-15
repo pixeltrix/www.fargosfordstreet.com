3f971e24de1f9eef89a0443ab4b76b8d72d5e399
Andrew White <andyw@pixeltrix.co.uk>
2013-05-15 11:04:35 +0100

    Install Redirect by Custom Field plugin

---
+++ public/wp-content/plugins/redirect-by-custom-field/readme.txt
@@ -0,0 +1,55 @@
+=== Redirect by Custom Field ===
+Contributors: mitchoyoshitaka
+Author: mitcho (Michael 芳貴 Erlewine)
+Author URI: http://mitcho.com/code/
+Donate link: http://tinyurl.com/donatetomitcho
+Tags: redirect, alias, custom field, URL
+Requires at least: 3.1
+Tested up to: 3.5
+Stable tag: 0.9
+
+Changes the URLs pointing to pages and posts which have a "redirect" custom field, using that entry as the URL instead.
+
+== Description ==
+
+Sometimes you have a need for having links to external URLs show up interspersed among your own pages or posts. This plugin lets you easily create such "redirecting" posts or pages by simply setting a custom field.
+
+**Usage**: Add the custom field with label "redirect" to any post or page. Enter the redirect target URL as the value of that custom field. Now all links on your site to that post or page will be replaced with that redirect URL! If you try to visit the permalink URL for that page, it will redirect the user to that redirect URL too.
+
+This plugin actually also works fine to redirect a URL to another URL on the same site as well.
+
+**Placeholders**: New in version 0.9: the strings `%home%` and `%site%` in URLs are replaced by the WordPress home URL and site URLs, respectively.
+
+Development of this plugin was supported by the [Arts at MIT](http://arts.mit.edu/).
+
+== Frequently Asked Questions ==
+
+If your question isn't here, ask your own question at [the Wordpress.org forums](http://wordpress.org/tags/redirect-by-custom-field?forum_id=10#postform). *Please do not email or tweet with questions.*
+
+== Changelog ==
+
+= 0.9 =
+* New placeholder function:
+	* `%home%` and `%site%` in URLs are replaced by the WordPress home URL and site URLs, respectively.
+	* New filter `redirect_by_custom_field_placeholders` to add or modify placeholders.
+
+= 0.8 =
+* Removed inappropriate `esc_url` which was messing up some redirects.
+* New user-definable constant `REDIRECT_BY_CUSTOM_FIELD_HTTP_STATUS`.
+
+= 0.7 =
+* Rewritten to use the `template_redirect` action instead, which catches some edge cases.
+
+= 0.6 =
+* WordPress's own permalinks in the WordPress admin now are actually displayed, together with the redirect URL.
+
+= 0.5 =
+* Fixed a potential PHP warning
+* Fixed an error in the query filter
+
+= 0.4 =
+* Fixed redirect in some direct URL access cases
+* Properly sanitizes redirect URLs
+
+= 0.3 =
+* First public release
---
+++ public/wp-content/plugins/redirect-by-custom-field/redirect-by-custom-field.php
@@ -0,0 +1,77 @@
+<?php
+/*
+Plugin Name: Redirect by Custom Field
+Description: Changes the URLs pointing to pages and posts which have a <code>redirect</code> custom field, using that entry as the URL instead.
+Author: mitcho (Michael 芳貴 Erlewine)
+Version: 0.9
+Author URI: http://mitcho.com/
+Donate link: http://tinyurl.com/donatetomitcho
+*/
+
+if ( !defined('REDIRECT_BY_CUSTOM_FIELD_HTTP_STATUS') )
+	define( 'REDIRECT_BY_CUSTOM_FIELD_HTTP_STATUS', 301 );
+
+// This section will redirect URL's when the real permalink is hit directly.
+// Note: I didn't use rewrite because I didn't want to have to keep track of all redirect mappings.
+add_action('template_redirect', 'redirect_header', 1);
+function redirect_header() {
+	global $wp_query;
+	if ( is_singular() &&
+		 ($id = get_queried_object_id()) &&
+		 ($link = get_redirect_url($id)) ) {
+		wp_redirect($link, REDIRECT_BY_CUSTOM_FIELD_HTTP_STATUS);
+		exit;
+	}
+}
+
+// This section will replace every instance of the permalink with the redirect URL.
+add_filter('page_link', 'redirect_by_custom_field', 10, 2);
+add_filter('post_link', 'redirect_by_custom_field', 10, 2);
+function redirect_by_custom_field($link, $postarg = null) {
+	global $post;
+	if ( is_admin() )
+		return $link;
+	
+	if (is_object($postarg))
+		$id = $postarg->ID;
+	else if (is_int($postarg))
+		$id = $postarg;
+	else if (is_object($post))
+		$id = $post->ID;
+	else
+		return $link;
+	
+	if ( $redirect = get_redirect_url($id) )
+		return $redirect;
+	
+	return $link;
+}
+
+// id must be int
+function get_redirect_url($id) {
+	static $placeholders;
+
+	if ( $redirect = get_post_meta( absint($id), 'redirect', true ) ) {
+
+		if ( !isset($placeholders) ) {
+			$placeholders = apply_filters( 'redirect_by_custom_field_placeholders', 
+				array(
+					'%home%' => get_home_url(),
+					'%site%' => get_site_url(),
+				)
+			);
+		}
+
+		return str_replace( array_keys($placeholders),
+			array_values($placeholders),
+			is_array($redirect) ? $redirect[0] : $redirect );
+	}
+	return false;
+}
+
+add_filter('get_sample_permalink_html', 'redirect_display_modifier', 10, 4);
+function redirect_display_modifier($return, $id, $new_title, $new_slug) {
+	if ( $redirect = get_redirect_url($id) )
+		$return = "<strong>" . __("Redirect:", 'redirect-by-custom-field') . "</strong> " . esc_html($redirect) . "<style>#titlediv {margin-bottom: 30px;}</style><br/>" . $return;
+	return $return;
+}
